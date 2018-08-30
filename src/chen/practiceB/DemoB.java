9baba33f4c25a76efeda28c1aab7a9b96ef1c03b
chezhuo <15161171791@163.com>
2018-08-30 09:25:33 +0800

    together the problems

--- src/chen/practiceB/DemoB.java
+++ src/chen/practiceB/DemoB.java
@@ -6,9 +6,12 @@ public class DemoB {
 		ListNode node1 = new ListNode(5);
 		ListNode node2 = new ListNode(4);
 		ListNode node3 = new ListNode(2);
-		node2.next = node3 ;
-		node1.next = node2 ;
-		node1.printListNode();
+//		node2.next = node3 ;
+//		node1.next = node2 ;
+//		node1.printListNode();
+		node3.next = node2 ;
+		node2.next = node1 ;
+		node3.printListNode();
 	}
 
 
