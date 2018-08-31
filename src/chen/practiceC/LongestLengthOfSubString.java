6ea7de48d3d52bfe82c8d7d488f8dc7db541da1d
chenzhuo1791 <15161171791@163.com>
2018-08-31 18:28:03 +0800

    practiceC - length of  longest-substring

---
+++ src/chen/practiceC/LongestLengthOfSubString.java
@@ -0,0 +1,46 @@
+package chen.practiceC;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestLengthOfSubString {
+
+	public static void main(String[] args){
+		String string = null ;
+		Integer result = lengthOfLongestSubString(string);
+		System.out.println("  @@ lengthOfLongestSubString : " + result);
+	}
+
+	public static Integer lengthOfLongestSubString(String string){
+		Integer result = 0 ;
+		if (string != null) {
+			Integer length = string.length();
+			for (int j = 0 ; j < length ; j++) {
+				for (int k = j + 1; k <= length; k++) {
+					boolean checkFlag = allUnique(string, j, k);
+					if (checkFlag == true) {
+						result = Math.max(result, k - j);
+					}
+				}
+			}
+		}
+		return result ;
+	}
+
+	public static boolean allUnique(String string , Integer start , Integer end){
+		boolean flag = false ;
+		Set<Character> set = new HashSet<Character>();
+		for (int i = start ; i < end ; i++) {
+			Character temp = string.charAt(i);
+			if (set.contains(temp)) {
+				return flag;
+			} else {
+				set.add(temp);
+			}
+		}
+		flag = true ;
+		return flag ;
+	}
+
+
+}
